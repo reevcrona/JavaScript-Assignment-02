d36a9a64b6a24c62fb39d419bca978ddb8217ba6
reevcrona <89569507+reevcrona@users.noreply.github.com>
2024-10-16 11:49:41 +0200

    done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("test");
+let userPrice = prompt("Please enter a price (example: $49.99)");
+
+
+
+let price = parseFloat(userPrice.slice(1));
+
+
+
+let newPrice = price * 0.9;
+
+
+console.log(`$${newPrice.toFixed(2)}`);
